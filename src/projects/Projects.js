c269bf4b895a74d617a37620fd2d0498a8c8b072
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2023-03-22 14:21:50 +0300

    add two projects

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -5,6 +5,8 @@ import Project from './project/Project';
 import todoImage from '../accets/image/todolist.png'
 import socialNetworkImage from '../accets/image/socialNetwork.png'
 import cardNetworkImage from '../accets/image/cardsNetwork.png'
+import calculatorImage from '../accets/image/calculator.png'
+import notesImage from '../accets/image/notes.png'
 import Title from "../common/components/title/Title";
 import {useTranslation} from "react-i18next";
 
@@ -19,13 +21,19 @@ const Projects = () => {
     const cardNetwork = {
         backgroundImage: `url(${cardNetworkImage})`,
     };
+    const calculatorNetwork = {
+        backgroundImage: `url(${calculatorImage})`,
+    };
+    const notesNetwork = {
+        backgroundImage: `url(${notesImage})`,
+    };
 
     return (
         <div className={style.projectsBlock} id="projects">
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title color={'#2c3e50'}
                        title={t('Projects title')}
-                      />
+                />
                 <div className={style.projects}>
                     <Project
                         style={socialNetwork}
@@ -33,21 +41,30 @@ const Projects = () => {
                         description={t('Project Social network description')}
                         link={'https://github.com/LyudmilaShch/Samurai_way_TS'}
                     />
-
                     <Project
                         style={todolistNetwork}
                         title={t('Project Todolist title')}
                         description={t('Project Todolist description')}
                         link={'https://github.com/LyudmilaShch/todolist-ts-main'}
                     />
-
                     <Project
                         style={cardNetwork}
                         title={t('Project Card learning title')}
                         description={t('Project Card learning description')}
                         link={'https://github.com/AlexKazakq/friday-project-cards'}
                     />
-
+                    <Project
+                        style={calculatorNetwork}
+                        title={t('Project Calculator title')}
+                        description={t('Project Calculator description')}
+                        link={'https://github.com/LyudmilaShch/calculator'}
+                    />
+                    <Project
+                        style={notesNetwork}
+                        title={t('Project Notes title')}
+                        description={t('Project Notes description')}
+                        link={'https://github.com/LyudmilaShch/metacommerce-test'}
+                    />
                 </div>
 
             </div>
